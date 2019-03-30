e505206f519c0c6a3707dd17df6e23c1aab039bf
Krzysztof Drzazga <krzysztof.drzazga@capgemini.com>
2019-03-30 15:22:18 +0100

    Simple music

---
+++ HelloScroll/MusicIrq.asm
@@ -0,0 +1,63 @@
+//Kick Assembler
+BasicUpstart2(start)
+
+.const InterruptStatusRegister1 = $d019
+.const InterruptStatusRegister2 = $d01a
+.const InterruptControlAndStatusRegister1 = $dc0d
+.const InterruptControlAndStatusRegister2 = $dd0d
+.const InterruptExecutionMSB = $fffe
+.const InterruptExecutionLSB = $ffff
+.const ProcessorPort = $0001
+
+			* = $0960
+start:		lda #$00
+			jsr music_init
+			sei
+			lda #%00110101
+			sta ProcessorPort
+			lda #<irq1
+			sta InterruptExecutionMSB
+			lda #>irq1
+			sta InterruptExecutionLSB
+
+			lda #%10000001
+			sta InterruptStatusRegister2
+			lda #%01111111
+			sta InterruptControlAndStatusRegister1
+			sta InterruptControlAndStatusRegister2
+
+			lda InterruptControlAndStatusRegister1
+			lda InterruptControlAndStatusRegister2
+			lda #$ff
+			sta InterruptStatusRegister1
+
+			cli
+			jmp *
+
+irq1:  		pha
+			txa
+			pha
+			tya
+			pha
+			lda #$ff
+			sta	InterruptStatusRegister1
+
+			jsr music_play
+			pla
+			tay
+			pla
+			tax
+			pla
+			rti
+
+			*=$1000 "Music"
+			.label music_init =*			// <- You can define label with any value (not just at the current pc position as in 'music_init:') 
+			.label music_play =*+3			// <- and that is useful here
+			.import binary "ode64.bin"
+
+//----------------------------------------------------------
+// A little macro
+.macro SetBorderColor(color) {		// <- This is how macros are defined
+	lda #color
+	sta $d020
+}
--- HelloScroll/Scroll.asm
+++ HelloScroll/Scroll.asm
@@ -1,6 +1,11 @@
 //for KickAssembler
 BasicUpstart2(mainProg)
 
+.const GETIN  =  $ffe4
+.const SCNKEY =  $ff9f
+.const SOFT_RESET = $fce2
+.const SPACE = 32
+	
 .const textMemoryBank = $0400
 .const screenControlRegister = $d016
 .const memorySetupRegister = $d018
@@ -8,8 +13,8 @@ BasicUpstart2(mainProg)
 
 .const textLineWidth = 40
 .const scrollLine = 24 * textLineWidth + textMemoryBank
-.const memorySetup = $17 //Bits #0-#2: Horizontal raster scroll. #3: Screen width; 0 = 38 columns; 1 = 40 columns. #4: 1 = Multicolor mode on.
-.const horizontalScroll = $c8 //
+.const memorySetup = %00010111 //Bits #0-#2: Horizontal raster scroll. #3: Screen width; 0 = 38 columns; 1 = 40 columns. #4: 1 = Multicolor mode on.
+.const horizontalScroll = $c8
 .const rasterLineForScroll = $ff - 13 //Raster line to generate interrupt at (bits #0-#7) - last textline of screen
 
 //Changing raster to narrow bottom of the screen allows for smooth scrolling 13 is the height of single textline
@@ -21,14 +26,14 @@ mainProg: 	{
 			lda #memorySetup
 			sta memorySetupRegister
 
-			// Wait for line $f2 and set d016	
+			// Wait for "raster scanner" to reach line $f2 (rasterLineForScroll) and set d016 (rasterLineCell)
 mainLoop:
 	        lda #rasterLineForScroll
 			cmp rasterLineCell
 			bne mainLoop
 			jsr SCROLL
 			
-			// Wait for line $ff and prepare next frame 
+			// Wait for line $ff (finalRasterLine) and prepare next frame 
 loop2:		lda #finalRasterLine
 			cmp rasterLineCell
 			bne loop2
@@ -38,20 +43,18 @@ loop2:		lda #finalRasterLine
 			jsr moveScroll
 			
 			jmp mainLoop
-}
 
-SCROLL:	{
-value:		lda #0									
+SCROLL:	
+			lda #0									
 			and #$07
 			ora #$c0
 			sta screenControlRegister
 			rts			
-}
 
-moveScroll: {
+moveScroll: 
 			// Step d016
-			dec SCROLL.value + 1			//<- We can access labels of other scopes this way!
-			lda SCROLL.value + 1
+			dec SCROLL + 1
+			lda SCROLL + 1
 			and #$07
 			cmp #$07
 			bne exit
@@ -59,7 +62,7 @@ moveScroll: {
 			// Move screen chars
 			ldx #0
 scrollLoop:
-	        lda scrollLine+1, x
+	        lda scrollLine + 1, x
 			sta scrollLine, x
 			inx
 			cpx #textLineWidth - 1
@@ -69,15 +72,22 @@ scrollLoop:
 count:		ldx #0
 			lda text, x
 			sta scrollLine + textLineWidth - 1
-			inx 
-			lda text, x
+			inx
+							
+			lda text, x			
 			cmp endText
 			bne over1
 			ldx #0
-over1:		stx count+1
+over1:		stx count + 1
 			
 exit:		rts
 			
+readFirst		
+quit:
+	jmp SOFT_RESET			
+	
 text:		.text "SCROLLING!!! SCROLLING!!! SCROLLLLLLLING!!! Hello to everyone.      "
 endText:	.byte $fe
+
+#import "MusicIrq.asm"
 }
